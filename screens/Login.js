122defa0b9ee9ff37d3a80b3815852d1ebc3c6db
Justin Dudley <justindudley99@gmail.com>
2021-12-10 16:09:49 -0500

    add handleChange and handleSubmit functionality

--- screens/Login.js
+++ screens/Login.js
@@ -1,10 +1,18 @@
 import React from 'react';
 import { Text, View } from 'react-native';
+import { useFormik } from 'formik';
 
 import Button from '../components/Button';
 import TextInput from '../components/TextInput';
 
 export default function Login() {
+
+    const {handleChange, handleSubmit, values } = useFormik({
+        initialValues: {email: 'tempYo', password: ''},
+        onSubmit: values => 
+        alert(`Email: ${values.email}, Password: ${values.password}`)
+    })
+
     return (
         <View
             style={{
@@ -38,6 +46,7 @@ export default function Login() {
                     keyboardAppearance='dark'
                     returnKeyType='next'
                     returnKeyLabel='next'
+                    onChangeText={handleChange('email')}  //add user input to the property, defined above, named "email" [WAS email: 'tempYo', IS NOW email: 'whatever the user just typed']
                 />
             </View>
             <View
@@ -56,11 +65,12 @@ export default function Login() {
                     keyboardAppearance='dark'
                     returnKeyType='go'
                     returnKeyLabel='go'
+                    onChangeText={handleChange('password')} // as with 'email' in block above, this string called 'password' is passed somehow into the block at the top where formik was introduced
                 />
             </View>
             <Button 
-                label='Login'
-                onPress={() => true}
+                label='Login (the GO button)'
+                onPress={handleSubmit} // press goes into formik-land
             />
         </View>
     )
